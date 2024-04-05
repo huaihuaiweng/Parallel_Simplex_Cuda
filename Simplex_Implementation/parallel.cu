29e169ad4210e70868dcc46685fb98b20d7cd804
Zain Darvesh <zaindarvesh531@gmail.com>
2024-04-04 22:07:45 -0400

    Added code to find the index with the most negative value in the final row, which contains the objective function parameters.

--- Simplex_Implementation/parallel.cu
+++ Simplex_Implementation/parallel.cu
@@ -1,4 +1,4 @@
-#include cuda.h
+#include <cuda.h>
 double **tableau;
 
 struct Compare_Max {
@@ -8,6 +8,7 @@ struct Compare_Max {
 
 int main(int argc, char** argv) {
     int nRow, nCol;
+    Compare_Max max = findMaxObjective(tableau, nRow, nCol);
     
 }
 
@@ -26,6 +27,46 @@ __global__ void findMaxObjectiveKernel(double *tableau, int nRow, int nCol, Comp
     }
 }
 
-Compare_Max findMaxfromObjectiveFunction() {
+Compare_Max findMaxObjective(double **tableau, int nRow, int nCol) {
+    Compare_Max max;
+    max.val = 0.0;
+    max.index = -1;
 
+    // Flatten the 2D array
+    double *flat_tableau = new double[nRow * nCol];
+    for (int i = 0; i < nRow; ++i) {
+        for (int j = 0; j < nCol; ++j) {
+            flat_tableau[i * nCol + j] = tableau[i][j];
+        }
+    }
+
+    // Allocate device memory
+    double *d_tableau;
+    Compare_Max *d_max;
+    cudaMalloc((void**)&d_tableau, nRow * nCol * sizeof(double));
+    cudaMalloc((void**)&d_max, sizeof(Compare_Max));
+
+    // Copy data to device
+    cudaMemcpy(d_tableau, flat_tableau, nRow * nCol * sizeof(double), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_max, &max, sizeof(Compare_Max), cudaMemcpyHostToDevice);
+
+    // Launch kernel
+    dim3 blockSize(256);
+    dim3 gridSize((nCol + blockSize.x - 1) / blockSize.x);
+    findMaxObjectiveKernel<<<gridSize, blockSize>>>(d_tableau, nRow, nCol, d_max);
+
+    // Copy result back to host
+    cudaMemcpy(&max, d_max, sizeof(Compare_Max), cudaMemcpyDeviceToHost);
+
+    // Free device memory
+    cudaFree(d_tableau);
+    cudaFree(d_max);
+
+    // Free host memory
+    delete[] flat_tableau;
+
+    // For debugging
+    cout << "max.index: " << max.index << endl;
+
+    return max;
 }
