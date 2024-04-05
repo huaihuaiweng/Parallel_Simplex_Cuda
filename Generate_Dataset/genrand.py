2fc0c69990a91697b981e82bc106a2a8f89072e2
huaihuaiweng <38502955+huaihuaiweng@users.noreply.github.com>
2024-04-04 20:39:46 -0400

    data generation script

--- Generate_Dataset/genrand.py
+++ Generate_Dataset/genrand.py
@@ -1,1 +1,35 @@
 import numpy as np
+from scipy.sparse import random, diags
+
+def genrand(m, n, d):
+    c = np.zeros(n)
+    A = np.array([])
+    b = np.array([])
+
+    pl = lambda x: (np.abs(x) + x) / 2
+
+    while np.any(c >= 0):
+        A = random(m, n, density=d, format='csr', random_state=42)
+        A.data *= 50
+        x = 10 * pl(np.random.rand(n))
+        u = diags((np.sign(pl(np.random.rand(m) - np.random.rand(m)))), 0) @ (np.random.rand(m) - np.random.rand(m))
+        b = A @ x
+        c = A.transpose() @ u + diags((np.ones((n)) - np.sign(pl(x))), 0) @ (10 * np.ones((n)))
+
+    f = c.transpose()
+    A = A.toarray()
+    b = np.transpose(b)
+    ppl = np.block([[A, np.eye(m), b.reshape(-1,1)], [f, np.zeros((1, m+1))]])
+
+    return ppl, f, A, b
+
+
+# Example usage
+# m = 5  # Number of constraints
+# n = 4  # Number of variables
+# d = 0.5  # Density of the constraint matrix
+# ppl, f, A, b = genrand(m, n, d)
+# print("PPL:", ppl)
+# print("f:", f)
+# print("A:", A)
+# print("b:", b)
