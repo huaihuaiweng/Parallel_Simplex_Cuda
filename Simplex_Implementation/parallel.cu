4d41dffd1b38b74f776a39efc4b19f291da69f04
Zain Darvesh <zaindarvesh531@gmail.com>
2024-04-05 00:50:22 -0400

    updated cuda implementation. Removed unneccessary method.

--- Simplex_Implementation/parallel.cu
+++ Simplex_Implementation/parallel.cu
@@ -4,8 +4,10 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#define NUM_THREADS = 256
 
 double* tableau_gpu;
+int blks;
 
 
 struct Compare_Max {
@@ -66,18 +68,22 @@ int main(int argc, char** argv) {
     cudaMalloc((void**)&tableau_gpu, nRow * nCol * sizeof(double));
     cudaMemCpy(tableau_gpu, tableau_cpu2, nRow * nCol * sizeof(double), cudaMemCpyHostToDevice);
 
+    // Launch kernel
+    Compare_Max max;
+    blks = (nCol * nRow + NUM_THREADS - 1) / NUM_THREADS;
+    findMaxObjectiveKernel<<<blks, NUM_THREADS>>>(tableau_gpu, nRow, nCol, max);
 
-
-    Compare_Max max = findMaxObjective(tableau_gpu, nRow, nCol);
-    print(max);
+  
+    print(max->val);
     
 }
 
 
 __global__ void findMaxObjectiveKernel(double *tableau, int nRow, int nCol, Compare_Max *max) {
     int j = threadIdx.x + blockIdx.x * blockDim.x;
-    if (j <= nCol) {
-        double val = tableau[nRow * nCol + j];
+    int arr_size = nRow * nCol;
+    if (j < size) {
+        double val = tableau[j];
         if (val < 0.0) {
             val = -val;
             atomicMax(&(max->val), val);
@@ -88,46 +94,46 @@ __global__ void findMaxObjectiveKernel(double *tableau, int nRow, int nCol, Comp
     }
 }
 
-Compare_Max findMaxObjective(double **tableau, int nRow, int nCol) {
-    Compare_Max max;
-    max.val = 0.0;
-    max.index = -1;
+// Compare_Max findMaxObjective(double *tableau, int nRow, int nCol) {
+//     Compare_Max max;
+//     max.val = 0.0;
+//     max.index = -1;
 
-    // Flatten the 2D array
-    double *flat_tableau = new double[nRow * nCol];
-    for (int i = 0; i < nRow; ++i) {
-        for (int j = 0; j < nCol; ++j) {
-            flat_tableau[i * nCol + j] = tableau[i][j];
-        }
-    }
+//     // Flatten the 2D array
+//     double *flat_tableau = new double[nRow * nCol];
+//     for (int i = 0; i < nRow; ++i) {
+//         for (int j = 0; j < nCol; ++j) {
+//             flat_tableau[i * nCol + j] = tableau[i][j];
+//         }
+//     }
 
-    // Allocate device memory
-    double *d_tableau;
-    Compare_Max *d_max;
-    cudaMalloc((void**)&d_tableau, nRow * nCol * sizeof(double));
-    cudaMalloc((void**)&d_max, sizeof(Compare_Max));
+//     // Allocate device memory
+//     double *d_tableau;
+//     Compare_Max *d_max;
+//     cudaMalloc((void**)&d_tableau, nRow * nCol * sizeof(double));
+//     cudaMalloc((void**)&d_max, sizeof(Compare_Max));
 
-    // Copy data to device
-    cudaMemcpy(d_tableau, flat_tableau, nRow * nCol * sizeof(double), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_max, &max, sizeof(Compare_Max), cudaMemcpyHostToDevice);
+//     // Copy data to device
+//     cudaMemcpy(d_tableau, flat_tableau, nRow * nCol * sizeof(double), cudaMemcpyHostToDevice);
+//     cudaMemcpy(d_max, &max, sizeof(Compare_Max), cudaMemcpyHostToDevice);
 
-    // Launch kernel
-    dim3 blockSize(256);
-    dim3 gridSize((nCol + blockSize.x - 1) / blockSize.x);
-    findMaxObjectiveKernel<<<gridSize, blockSize>>>(d_tableau, nRow, nCol, d_max);
+//     // Launch kernel
+//     dim3 blockSize(256);
+//     dim3 gridSize((nCol + blockSize.x - 1) / blockSize.x);
+//     findMaxObjectiveKernel<<<gridSize, blockSize>>>(d_tableau, nRow, nCol, d_max);
 
-    // Copy result back to host
-    cudaMemcpy(&max, d_max, sizeof(Compare_Max), cudaMemcpyDeviceToHost);
+//     // Copy result back to host
+//     cudaMemcpy(&max, d_max, sizeof(Compare_Max), cudaMemcpyDeviceToHost);
 
-    // Free device memory
-    cudaFree(d_tableau);
-    cudaFree(d_max);
+//     // Free device memory
+//     cudaFree(d_tableau);
+//     cudaFree(d_max);
 
-    // Free host memory
-    delete[] flat_tableau;
+//     // Free host memory
+//     delete[] flat_tableau;
 
-    // For debugging
-    cout << "max.index: " << max.index << endl;
+//     // For debugging
+//     cout << "max.index: " << max.index << endl;
 
-    return max;
-}
+//     return max;
+// }
